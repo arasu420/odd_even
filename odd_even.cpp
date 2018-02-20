f170ba1160bc1dce32e3fe28e424b7e078ad67a5
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-02-20 16:20:10 +0530

    Add files via upload

---
+++ odd_even.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+int x;
+cout << "Please enter an integer: ";
+cin >> x;
+if (x%2==0)
+{
+cout << "The number u entered is even" << endl;
+}
+else
+{
+cout << "The number u entered is odd" << endl;
+}
+return 0;
+}
